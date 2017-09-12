2ae4dd7a0fc2e83995fc894b1f6b9835c561e125
wy54224 <752983036@qq.com>
2017-09-12 15:27:57 +0800

    增加人机对战功能

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -28,172 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new TicTacToe.UButton();
-            this.button2 = new TicTacToe.UButton();
-            this.button3 = new TicTacToe.UButton();
-            this.button4 = new TicTacToe.UButton();
-            this.button5 = new TicTacToe.UButton();
-            this.button6 = new TicTacToe.UButton();
-            this.button7 = new TicTacToe.UButton();
-            this.button8 = new TicTacToe.UButton();
-            this.button9 = new TicTacToe.UButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button10 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 75);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.OnClick);
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.Location = new System.Drawing.Point(71, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 75);
-            this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.OnClick);
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Transparent;
-            this.button3.Location = new System.Drawing.Point(143, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 75);
-            this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.OnClick);
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.Transparent;
-            this.button4.Location = new System.Drawing.Point(0, 71);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 75);
-            this.button4.TabIndex = 5;
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.OnClick);
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.Color.Transparent;
-            this.button5.Location = new System.Drawing.Point(71, 71);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 75);
-            this.button5.TabIndex = 4;
-            this.button5.UseVisualStyleBackColor = false;
-            this.button5.Click += new System.EventHandler(this.OnClick);
-            // 
-            // button6
-            // 
-            this.button6.BackColor = System.Drawing.Color.Transparent;
-            this.button6.Location = new System.Drawing.Point(143, 71);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 75);
-            this.button6.TabIndex = 3;
-            this.button6.UseVisualStyleBackColor = false;
-            this.button6.Click += new System.EventHandler(this.OnClick);
-            // 
-            // button7
-            // 
-            this.button7.BackColor = System.Drawing.Color.Transparent;
-            this.button7.Location = new System.Drawing.Point(0, 140);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 75);
-            this.button7.TabIndex = 8;
-            this.button7.UseVisualStyleBackColor = false;
-            this.button7.Click += new System.EventHandler(this.OnClick);
-            // 
-            // button8
-            // 
-            this.button8.BackColor = System.Drawing.Color.Transparent;
-            this.button8.Location = new System.Drawing.Point(71, 140);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(75, 75);
-            this.button8.TabIndex = 7;
-            this.button8.UseVisualStyleBackColor = false;
-            this.button8.Click += new System.EventHandler(this.OnClick);
-            // 
-            // button9
-            // 
-            this.button9.BackColor = System.Drawing.Color.Transparent;
-            this.button9.Location = new System.Drawing.Point(143, 140);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(75, 75);
-            this.button9.TabIndex = 6;
-            this.button9.UseVisualStyleBackColor = false;
-            this.button9.Click += new System.EventHandler(this.OnClick);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(224, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 20);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "现在轮到：";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(252, 32);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 10;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(237, 89);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 15);
-            this.label2.TabIndex = 11;
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(228, 140);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(105, 39);
-            this.button10.TabIndex = 12;
-            this.button10.Text = "重新开始";
-            this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.ReFresh);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(345, 213);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button10 = new System.Windows.Forms.Button();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.button9 = new TicTacToe.UButton();
+			this.button8 = new TicTacToe.UButton();
+			this.button7 = new TicTacToe.UButton();
+			this.button6 = new TicTacToe.UButton();
+			this.button5 = new TicTacToe.UButton();
+			this.button4 = new TicTacToe.UButton();
+			this.button3 = new TicTacToe.UButton();
+			this.button2 = new TicTacToe.UButton();
+			this.button1 = new TicTacToe.UButton();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label1.Location = new System.Drawing.Point(224, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(109, 20);
+			this.label1.TabIndex = 9;
+			this.label1.Text = "现在轮到：";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(252, 32);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 10;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(237, 89);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(0, 15);
+			this.label2.TabIndex = 11;
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(228, 182);
+			this.button10.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(105, 31);
+			this.button10.TabIndex = 12;
+			this.button10.Text = "重新开始";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.ReFresh);
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Checked = true;
+			this.radioButton1.Location = new System.Drawing.Point(228, 113);
+			this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(88, 19);
+			this.radioButton1.TabIndex = 13;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Tag = 1;
+			this.radioButton1.Text = "人人对战";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			this.radioButton1.CheckedChanged += new System.EventHandler(this.playStatus);
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(228, 135);
+			this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(88, 19);
+			this.radioButton2.TabIndex = 14;
+			this.radioButton2.Tag = 2;
+			this.radioButton2.Text = "电脑先手";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			this.radioButton2.CheckedChanged += new System.EventHandler(this.playStatus);
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Location = new System.Drawing.Point(228, 157);
+			this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(73, 19);
+			this.radioButton3.TabIndex = 15;
+			this.radioButton3.Tag = 3;
+			this.radioButton3.Text = "人先手";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			this.radioButton3.CheckedChanged += new System.EventHandler(this.playStatus);
+			// 
+			// button9
+			// 
+			this.button9.BackColor = System.Drawing.Color.Transparent;
+			this.button9.Location = new System.Drawing.Point(143, 140);
+			this.button9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(75, 75);
+			this.button9.TabIndex = 6;
+			this.button9.UseVisualStyleBackColor = false;
+			this.button9.Click += new System.EventHandler(this.OnClick);
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.Transparent;
+			this.button8.Location = new System.Drawing.Point(71, 140);
+			this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(75, 75);
+			this.button8.TabIndex = 7;
+			this.button8.UseVisualStyleBackColor = false;
+			this.button8.Click += new System.EventHandler(this.OnClick);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.Transparent;
+			this.button7.Location = new System.Drawing.Point(0, 140);
+			this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(75, 75);
+			this.button7.TabIndex = 8;
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.OnClick);
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.Transparent;
+			this.button6.Location = new System.Drawing.Point(143, 71);
+			this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 75);
+			this.button6.TabIndex = 3;
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Click += new System.EventHandler(this.OnClick);
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.Transparent;
+			this.button5.Location = new System.Drawing.Point(71, 71);
+			this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 75);
+			this.button5.TabIndex = 4;
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.OnClick);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.Transparent;
+			this.button4.Location = new System.Drawing.Point(0, 71);
+			this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 75);
+			this.button4.TabIndex = 5;
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.OnClick);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.Transparent;
+			this.button3.Location = new System.Drawing.Point(143, 0);
+			this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 75);
+			this.button3.TabIndex = 2;
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.OnClick);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Transparent;
+			this.button2.Location = new System.Drawing.Point(71, 0);
+			this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 75);
+			this.button2.TabIndex = 1;
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.OnClick);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.Location = new System.Drawing.Point(0, 0);
+			this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 75);
+			this.button1.TabIndex = 0;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.OnClick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(345, 212);
+			this.Controls.Add(this.radioButton3);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -212,6 +271,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,78 +12,220 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        private struct Point
+        {
+            int x, y;
+        }
+
         private int[] isClick;
+        private int iPlayStatus;
         private bool player;
-        private const bool onTheOffensive = false;
+        private const bool onTheOffensive = true;
         public Form1()
         {
             InitializeComponent();
             isClick = new int[9];
             player = onTheOffensive;
+            iPlayStatus = 1;
             pictureBox1.Image = player ? Properties.Resources.player1 : Properties.Resources.player2;
         }
 
-        private void WinnerCheck()
+        private void Computer()
+        {
+            int i, max, maxi;
+			Button btn;
+            max = -2;
+            maxi = -1;
+            for(i = 0; i < 9; ++i)
+                if(isClick[i] == 0)
+                {
+                    int tmp;
+                    isClick[i] = 4;
+					if (WinnerCheck(false) == 4) tmp = 1;
+					else
+					if (WinnerCheck(false) == 3) tmp = 0;
+					else
+                    tmp = -dfs(false);
+					//Console.Write((i + 1).ToString() + ":" + tmp.ToString() + " ");
+                    if(tmp > max)
+                    {
+                        max = tmp;
+                        maxi = i;
+                    }
+                    isClick[i] = 0;
+                }
+			//Console.WriteLine(" ");
+			switch (maxi)
+			{
+				case 0:
+					btn = button1;
+					break;
+				case 1:
+					btn = button2;
+					break;
+				case 2:
+					btn = button3;
+					break;
+				case 3:
+					btn = button4;
+					break;
+				case 4:
+					btn = button5;
+					break;
+				case 5:
+					btn = button6;
+					break;
+				case 6:
+					btn = button7;
+					break;
+				case 7:
+					btn = button8;
+					break;
+				case 8:
+					btn = button9;
+					break;
+				default:
+					btn = null;
+					break;
+			}
+			btn.Image = Properties.Resources.player2;
+			pictureBox1.Image = Properties.Resources.player1;
+			btn.Enabled = false;
+			isClick[maxi] = 4;
+		}
+
+        private int dfs(bool isComputer)
+        {
+            int max = -1, i, now;
+            bool mustPut, isTrace;
+            mustPut = false;
+			isTrace = false;
+            if (isComputer) now = 4; else now = 1;
+            for(i = 0; i < 9; ++i)
+                if(isClick[i] == 0)
+                {
+                    isClick[i] = now;
+                    if(WinnerCheck(false) == now)
+                    {
+                        mustPut = true;
+                        isClick[i] = 0;
+                        break;
+                    }else
+					if(WinnerCheck(false) == 3)
+					{
+						isTrace = true;
+						isClick[i] = 0;
+						break;
+					}
+                    isClick[i] = 0;
+                }
+            if (mustPut) return 1;
+			if (isTrace) return 0;
+			max = -1;
+			for(i = 0; i < 9; ++i)
+				if(isClick[i] == 0)
+				{
+					isClick[i] = now;
+					max = Math.Max(max, -dfs(!isComputer));
+					isClick[i] = 0;
+				}
+            return max;
+        }
+
+        private int WinnerCheck(bool changeStatus)
         {
             int Winner;
             int sum;
+            bool bIsClick;
+            int i;
             Winner = 0;
             sum = 0;
 
             sum = isClick[0] + isClick[1] + isClick[2];
             if(sum == 3)Winner = 1;
             else
-            if(sum == 12)Winner = 2;
+            if(sum == 12)Winner = 4;
 
             sum = isClick[0] + isClick[4] + isClick[8];
             if(sum == 3)Winner = 1;
             else
-            if(sum == 12) Winner = 2;
+            if(sum == 12) Winner = 4;
 
             sum = isClick[0] + isClick[3] + isClick[6];
             if (sum == 3) Winner = 1;
             else
-            if (sum == 12) Winner = 2;
+            if (sum == 12) Winner = 4;
 
             sum = isClick[2] + isClick[5] + isClick[8];
             if (sum == 3) Winner = 1;
             else
-            if (sum == 12) Winner = 2;
+            if (sum == 12) Winner = 4;
 
             sum = isClick[2] + isClick[4] + isClick[6];
             if (sum == 3) Winner = 1;
             else
-            if (sum == 12) Winner = 2;
+            if (sum == 12) Winner = 4;
 
             sum = isClick[3] + isClick[4] + isClick[5];
             if (sum == 3) Winner = 1;
             else
-            if (sum == 12) Winner = 2;
+            if (sum == 12) Winner = 4;
 
             sum = isClick[1] + isClick[4] + isClick[7];
             if (sum == 3) Winner = 1;
             else
-            if (sum == 12) Winner = 2;
+            if (sum == 12) Winner = 4;
 
             sum = isClick[6] + isClick[7] + isClick[8];
             if (sum == 3) Winner = 1;
             else
-            if (sum == 12) Winner = 2;
+            if (sum == 12) Winner = 4;
+
+            bIsClick = true;
+            for(i = 0; i < 9; ++i)
+                if (isClick[i] == 0)
+                {
+                    bIsClick = false;
+                    break;
+                }
+            if (bIsClick)
+            {
+                if (changeStatus)
+                {
+                    label2.Text = "Trace!";
+                    pictureBox1.Image = null;
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button6.Enabled = false;
+                    button5.Enabled = false;
+                    button4.Enabled = false;
+                    button9.Enabled = false;
+                    button8.Enabled = false;
+                    button7.Enabled = false;
+                }
+                return 3;
+            }
 
             if (Winner != 0)
             {
-                label2.Text = "Winner :" + (Winner == 1 ? "×" : "○");
-                pictureBox1.Image = null;
-                button1.Enabled = false;
-                button2.Enabled = false;
-                button3.Enabled = false;
-                button6.Enabled = false;
-                button5.Enabled = false;
-                button4.Enabled = false;
-                button9.Enabled = false;
-                button8.Enabled = false;
-                button7.Enabled = false;
+                if (changeStatus)
+                {
+                    label2.Text = "Winner :" + (Winner == 1 ? "×" : "○");
+                    pictureBox1.Image = null;
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button6.Enabled = false;
+                    button5.Enabled = false;
+                    button4.Enabled = false;
+                    button9.Enabled = false;
+                    button8.Enabled = false;
+                    button7.Enabled = false;
+                }
+                return Winner;
             }
+            return 0;
         }
 
         private void OnClick(object sender, EventArgs e)
@@ -95,18 +237,20 @@ namespace TicTacToe
                 isClick[button.Name[6] - '1'] = 1;
                 button.Image = Properties.Resources.player1;
                 pictureBox1.Image = Properties.Resources.player2;
+                if (iPlayStatus == 1) player = !player;else
+                if (WinnerCheck(true) != 3)Computer();
             }
             else
             {
                 isClick[button.Name[6] - '1'] = 4;
                 button.Image = Properties.Resources.player2;
                 pictureBox1.Image = Properties.Resources.player1;
+                player = !player;
             }
-            WinnerCheck();
-            player = !player;
+            WinnerCheck(true);
         }
 
-        private void ReFresh(object sender, EventArgs e)
+        private void refresh()
         {
             Array.Clear(isClick, 0, 9);
             player = onTheOffensive;
@@ -129,6 +273,25 @@ namespace TicTacToe
             button8.Image = null;
             button7.Enabled = true;
             button7.Image = null;
+        }
+
+        private void ReFresh(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void playStatus(object sender, EventArgs e)
+        {
+            RadioButton rbt = (RadioButton)sender;
+            if (rbt.Checked == true) iPlayStatus = (int)rbt.Tag;
+            refresh();
+            if (iPlayStatus == 2)
+            {
+                isClick[4] = 4;
+                button5.Image = Properties.Resources.player1;
+				button5.Enabled = false;
+                pictureBox1.Image = Properties.Resources.player2;
+            }
         }
     }
 }
