32b7b4f2771e326660ab4815594b3aa010d802c8
wy54224 <752983036@qq.com>
2017-10-30 16:03:04 +0800

    Add chess eating

--- ChineseChess/Controller.cs
+++ ChineseChess/Controller.cs
@@ -65,17 +65,21 @@ namespace ChineseChess
 					typePos[model[i, j].Location.X, model[i, j].Location.Y] = j;
 				}
 			chessboard.MouseClick += (object sender, MouseEventArgs e) => {
+				timer.Enabled = false;
 				if(currentPlayer != Player.None && currentChess != ChessType.None)
 				{
-					timer.Enabled = false;
 					model[currentPlayer, currentChess].Live = true;
 					PictureBox tmp = (PictureBox)sender;
 					double length = (double)tmp.Size.Width / 9;
 					int x = e.Location.X, y = e.Location.Y;
 					x = (int)Math.Round((x - length / 2) / length);
 					y = (int)Math.Round((y - length / 2) / length);
-					MessageBox.Show(x.ToString() + " " + y.ToString());
-					LogicCheck(x, y);
+					if ((x != model[currentPlayer, currentChess].Location.X ||
+					y != model[currentPlayer, currentChess].Location.Y) && LogicCheck(x, y))
+					{
+
+					}
+					//MessageBox.Show(x.ToString() + " " + y.ToString());
 					currentPlayer = Player.None;
 					currentChess = ChessType.None;
 				}
@@ -86,37 +90,153 @@ namespace ChineseChess
 		{
 			if (currentPlayer != player || currentChess != type)
 			{
-				timer.Enabled = true;
-				if(currentPlayer != Player.None && currentChess != ChessType.None)
+				timer.Enabled = false;
+				if (currentPlayer != Player.None && currentChess != ChessType.None)
+				{
 					model[currentPlayer, currentChess].Live = true;
+					if (currentPlayer != player)
+					{
+						if (LogicCheck(model[player, type].Location.X, model[player, type].Location.Y))
+						{
+							currentPlayer = Player.None;
+							currentChess = ChessType.None;
+						}
+					} else {
+						timer.Enabled = true;
+						currentPlayer = player;
+						currentChess = type;
+					}
+				}
+				else
+				{
+					timer.Enabled = true;
+					currentPlayer = player;
+					currentChess = type;
+				}
 			}
 			else
+			{
+				if (timer.Enabled)
+				{
+					currentPlayer = Player.None;
+					currentChess = ChessType.None;
+				}
 				timer.Enabled = !timer.Enabled;
-			currentPlayer = player;
-			currentChess = type;
+			}
 		}
 
 		//各种走法判断、吃子判断等等
-		private void LogicCheck(int x, int y)
+		private bool LogicCheck(int x, int y)
 		{
+			if (playPos[x, y] == currentPlayer) return false;
+			int currentX = model[currentPlayer, currentChess].Location.X, currentY = model[currentPlayer, currentChess].Location.Y;
 			switch (currentChess)
 			{
 				case ChessType.Rook1:
 				case ChessType.Rook2:
-					if(x == model[currentPlayer, currentChess].Location.X)
+				#region 車判断逻辑
+					if (x == currentX)
 					{
-						int miny = y, maxy = model[currentPlayer, currentChess].Location.Y, i;
+						int miny = y, maxy = currentY;
 						if (miny > maxy) Swap(ref miny, ref maxy);
 						miny += 1;
+						for (int i = miny; i < maxy; ++i)
+							if (playPos[x, i] != Player.None && typePos[x, i] != ChessType.None)
+								return false; ;
 					}
 					else
-					if(y == model[currentPlayer, currentChess].Location.Y)
+					if (y == currentY)
 					{
-						
+						int minx = x, maxx = currentX;
+						if (minx > maxx) Swap(ref minx, ref maxx);
+						minx += 1;
+						for (int i = minx; i < maxx; ++i)
+							if (playPos[i, y] != Player.None && typePos[i, y] != ChessType.None)
+								return false;
+					}
+					else
+						return false;
+					if(playPos[x, y] != Player.None && typePos[x, y] != ChessType.None)
+					{
+						Player tmpPlayer = playPos[x, y];
+						ChessType tmpType = typePos[x, y];
+						model[tmpPlayer, tmpType].Live = false;
 					}
 					break;
+				#endregion
+				case ChessType.Knight1:
+				case ChessType.Knight2:
+				#region 马判断逻辑
+					if (currentY == y + 2 && (currentX == x + 1 || currentX + 1 == x))
+					{
+						if (playPos[currentX, currentY - 1] != Player.None && typePos[currentX, currentY - 1] != ChessType.None)
+							return false;
+					}
+					else
+					if (currentX == x + 2 && (currentY == y + 1 || currentY + 1 == y))
+					{
+						if (playPos[currentX - 1, currentY] != Player.None && typePos[currentX - 1, currentY] != ChessType.None)
+							return false;
+					}
+					else
+					if (currentY + 2 == y && (currentX == x + 1 || currentX + 1 == x))
+					{
+						if (playPos[currentX, currentY + 1] != Player.None && typePos[currentX, currentY + 1] != ChessType.None)
+							return false;
+					}
+					else
+					if (currentX + 2 == x && (currentY == y + 1 || currentY + 1 == y))
+					{
+						if (playPos[currentX + 1, currentY] != Player.None && typePos[currentX + 1, currentY] != ChessType.None)
+							return false;
+					}
+					else
+						return false;
+					if (playPos[x, y] != Player.None && typePos[x, y] != ChessType.None)
+					{
+						Player tmpPlayer = playPos[x, y];
+						ChessType tmpType = typePos[x, y];
+						model[tmpPlayer, tmpType].Live = false;
+					}
+					break;
+				#endregion
+				case ChessType.Elephant1:
+				case ChessType.Elephant2:
+				#region 象/相逻辑判断
+
+					break;
+				#endregion
+				case ChessType.Mandarin1:
+				case ChessType.Mandarin2:
+				#region 士/仕逻辑判断
+					break;
+				#endregion
+				case ChessType.Cannon1:
+				case ChessType.Cannon2:
+				#region 炮逻辑判断
+					break;
+				#endregion
+				case ChessType.King:
+				#region 将/帅逻辑判断
+					break;
+				#endregion
+				case ChessType.Pawn1:
+				case ChessType.Pawn2:
+				case ChessType.Pawn3:
+				case ChessType.Pawn4:
+				case ChessType.Pawn5:
+				#region 兵/卒逻辑判断
+					break;
+				#endregion
+				default:
+					return false;
 			}
+			playPos[model[currentPlayer, currentChess].Location.X, model[currentPlayer, currentChess].Location.Y] = Player.None;
+			typePos[model[currentPlayer, currentChess].Location.X, model[currentPlayer, currentChess].Location.Y] = ChessType.None;
+			playPos[x, y] = currentPlayer;
+			typePos[x, y] = currentChess;
 			model[currentPlayer, currentChess].Location = new Point(x, y);
+			return true;
 		}
 	}
 }
