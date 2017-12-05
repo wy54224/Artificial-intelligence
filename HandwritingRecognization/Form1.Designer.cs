ccb37db3bcf8007e4df9e5da5238905f5713b061
wy54224 <752983036@qq.com>
2017-12-05 21:57:48 +0800

    Add Handwriting Recognization

---
+++ HandwritingRecognization/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace HandwritingRecognization
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.WritingArea = new System.Windows.Forms.PictureBox();
+			this.Check = new System.Windows.Forms.Button();
+			this.Clear = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.WritingArea)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// WritingArea
+			// 
+			this.WritingArea.Location = new System.Drawing.Point(12, 12);
+			this.WritingArea.Name = "WritingArea";
+			this.WritingArea.Size = new System.Drawing.Size(340, 340);
+			this.WritingArea.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.WritingArea.TabIndex = 0;
+			this.WritingArea.TabStop = false;
+			this.WritingArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+			this.WritingArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
+			this.WritingArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnMouseUp);
+			// 
+			// Check
+			// 
+			this.Check.Location = new System.Drawing.Point(394, 40);
+			this.Check.Name = "Check";
+			this.Check.Size = new System.Drawing.Size(107, 43);
+			this.Check.TabIndex = 1;
+			this.Check.Text = "识别";
+			this.Check.UseVisualStyleBackColor = true;
+			this.Check.Click += new System.EventHandler(this.Recognization);
+			// 
+			// Clear
+			// 
+			this.Clear.Location = new System.Drawing.Point(394, 100);
+			this.Clear.Name = "Clear";
+			this.Clear.Size = new System.Drawing.Size(107, 43);
+			this.Clear.TabIndex = 2;
+			this.Clear.Text = "清空";
+			this.Clear.UseVisualStyleBackColor = true;
+			this.Clear.Click += new System.EventHandler(this.OnClear);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(542, 373);
+			this.Controls.Add(this.Clear);
+			this.Controls.Add(this.Check);
+			this.Controls.Add(this.WritingArea);
+			this.Name = "Form1";
+			this.Text = "HandwritingRecognization";
+			((System.ComponentModel.ISupportInitialize)(this.WritingArea)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox WritingArea;
+		private System.Windows.Forms.Button Check;
+		private System.Windows.Forms.Button Clear;
+	}
+}
+
---
+++ HandwritingRecognization/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HandwritingRecognization
+{
+	public partial class Form1 : Form
+	{
+		private Bitmap WritingCanvas;
+		private bool isClick;
+		private Point lastPoint;
+		private Graphics go;
+		private Pen DrawPen;
+		public Form1()
+		{
+			InitializeComponent();
+			//将绘图区长和宽设置为16的倍数，方便计算
+			WritingArea.Width = WritingArea.Width / 32 * 32;
+			WritingArea.Height = WritingArea.Height / 32 * 32;
+			WritingCanvas = new Bitmap(32, 32);
+			WritingArea.Image = WritingCanvas;
+			isClick = false;
+			lastPoint = new Point();
+			go = Graphics.FromImage(WritingCanvas);
+			go.Clear(Color.White);
+			DrawPen = new Pen(Color.Black);
+		}
+
+		private void OnMouseDown(object sender, MouseEventArgs e)
+		{
+			isClick = true;
+			lastPoint.X = e.X * 32 / WritingArea.Width;
+			lastPoint.Y = e.Y * 32 / WritingArea.Height;
+		}
+
+		private void OnMouseMove(object sender, MouseEventArgs e)
+		{
+			if (isClick && e.X < WritingArea.Width && e.Y < WritingArea.Height && e.X >= 0 && e.Y >= 0)
+			{
+				Point newPoint = new Point(e.X * 32 / WritingArea.Width, e.Y * 32 / WritingArea.Height);
+				go.DrawLine(DrawPen, lastPoint, newPoint);
+				WritingArea.Invalidate();
+				lastPoint = newPoint;
+			}
+		}
+
+		private void OnMouseUp(object sender, MouseEventArgs e)
+		{
+			isClick = false;
+			go.DrawLine(DrawPen, lastPoint, new Point(e.X * 32 / WritingArea.Width, e.Y * 32 / WritingArea.Height));
+			WritingArea.Invalidate();
+		}
+
+		private void OnClear(object sender, EventArgs e)
+		{
+			go.Clear(Color.White);
+			WritingArea.Invalidate();
+		}
+
+		private int GetAns(ref double[] x)
+		{
+			return 0;
+		}
+
+		private void Recognization(object sender, EventArgs e)
+		{
+			double []x;
+			x = new double[1024];
+			for (int i = 0; i < 32; ++i)
+				for (int j = 0; j < 32; ++j)
+				{
+					Color tmp = WritingCanvas.GetPixel(i, j);
+					x[i * 32 + j] = (tmp.R > 128 &&  tmp.G > 128 &&  tmp.B > 128) ? 0 : 1;
+				}
+			int result = GetAns(ref x);
+			for (int i = 0; i < 32; ++i)
+				for (int j = 0; j < 32; ++j)
+					WritingCanvas.SetPixel(i, j, x[i * 32 + j] > 0.5 ? Color.Green : Color.White);
+			WritingArea.Invalidate();
+		}
+	}
+}
